bb6a70d5af52b48f05b91ca9ab6aabb10b80d200
Lauren Capelluto <lauren@rigetti.com>
2018-12-02 20:43:29 -0800

    Get beadies and threads to display, rotate beadies (theres a skew, mapping needs to be updated

--- cl-knot.lisp
+++ cl-knot.lisp
@@ -39,7 +39,7 @@
 
 (defun id-difference (beadie0 beadie1)
   "Return the absolute difference of the beadie IDs, per index."
-  (dolist (index '(0 1 2))
+  (loop :for index :in '(0 1 2)
     :collect (id-index-difference index beadie0 beadie1)))
 
 (defun threadp (beadie0 beadie1)
@@ -121,11 +121,18 @@
                   (nth (random 2) planes))))
   knot)
 
-(defun untangledp (knot)
+(defgeneric untangledp (input)
+  (:documentation "T if the input is in the solution state, NIL otherwise."))
+
+(defmethod untangledp ((beadie beadie))
+  "Is the beadie in the solution state?"
+  (equal (id beadie)
+         (list (x-pos beadie) (y-pos beadie) (z-pos beadie))))
+
+(defmethod untangledp ((knot knot))
   "Is the knot untangled (i.e. in the solution state)?"
   (loop :for beadie :in (knot-beadies knot)
-        :when (not (equal (id beadie)
-                          (list (x-pos beadie) (y-pos beadie) (z-pos beadie))))
+        :when (not (untangledp beadie))
           :do (return nil)
         :finally (return t)))
 
@@ -157,7 +164,7 @@ The defined function is called ROTATE-AXIS where AXIS is replaced by its value, 
          (axis1-pos (intern (format nil "~a-POS" axis1)))
          (axis2-pos (intern (format nil "~a-POS" axis2))))
     `(defgeneric ,func-name (knot beadie direction)
-       (:documentation "TODO")
+       (:documentation "Rotate the given BEADIE around the center of the KNOT along the axis ,axis and in the direction DIRECTION.")
        (:method ((knot knot) beadie direction)
          (let* ((ax1-center (,(intern (format nil "POINT-~a" axis1)) (center knot)))
                 (ax2-center (,(intern (format nil "POINT-~a" axis2)) (center knot)))
@@ -168,42 +175,49 @@ The defined function is called ROTATE-AXIS where AXIS is replaced by its value, 
                                 1))
                 (ax2-sign (* -1 ax1-sign)))
            (setf (,axis1-pos beadie)
-                 (+ ax1-center
-                    (* ax1-sign
-                       (- ax2-beadie-pos ax2-center))))
+                 (floor (+ ax1-center
+                           (* ax1-sign
+                              (- ax2-beadie-pos ax2-center)))))
            (setf (,axis2-pos beadie)
-                 (+ ax2-center
-                    (* ax2-sign
-                       (- ax1-beadie-pos ax1-center)))))))))
+                 (floor (+ ax2-center
+                           (* ax2-sign
+                              (- ax1-beadie-pos ax1-center))))))))))
 
 (make-axis-rotation x)
 (make-axis-rotation y)
 (make-axis-rotation z)
-
-(defun move-beadie (knot beadie section direction)
-  "Modify a single beadie's location as part of a move."
-  (cond ((equal section 'earth)
-         (rotate-x knot beadie direction))
-        ((equal section 'wind)
-         (rotate-y knot beadie direction))
-        ((equal section 'fire)
-         (rotate-z knot beadie direction))))
 
 (defgeneric apply-move (knot section direction plane)
   (:documentation "Act a complete move upon the knot structure.")
 
   (:method ((knot knot) section direction plane)
     (dolist (beadie (knot-beadies knot))
-      :when (= plane (funcall section beadie))
-      :do (move-beadie knot beadie section direction)))
+      (cond ((and  (equal section 'earth)
+                   (= plane (x-pos beadie)))
+             (rotate-x knot beadie direction))
+            ((and (equal section 'wind)
+                  (= plane (y-pos beadie)))
+             (rotate-y knot beadie direction))
+            ((and (equal section 'fire)
+                  (= plane (z-pos beadie)))
+             (rotate-z knot beadie direction)))))
 
   (:method ((knot tiny-knot) section direction plane)
     ;; The normal dimension is 2, but the tiny knot is, well, tiny.
-    (when (= plane 2) (setf plane 1))
+    (when (= plane 2)
+      (call-next-method knot section direction 1))
     (call-next-method)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Graphics ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defparameter *beadie-scale* 150)
+(defparameter *x-offset* 300)
+(defparameter *y-offset* 275)
+(defparameter *rx-theta* (/ pi 5))
+(defparameter *ry-theta* (/ (* 5 pi) 6))
+(defparameter *rz-theta* 0)
+(defparameter *teal* (clim:make-rgb-color (/ 10 225) (/ 212 255) (/ 212 255)))
 
 (clim:define-application-frame pusheens-home ()
   ((knot :initarg :knot
@@ -227,24 +241,50 @@ The defined function is called ROTATE-AXIS where AXIS is replaced by its value, 
               app (clim:horizontally () int
                                      F)))))
 
+(defun beadie-to-sheet-x (beadie)
+  "Transform the beadie's X Y Z coordinates to the X coordinates of the sheet to be used by CLIM."
+  (let* ((rotated-x (- (* (x-pos beadie) (cos *rx-theta*))
+                       (* (z-pos beadie) (sin *rx-theta*))))
+         (scaled-x (* *beadie-scale* rotated-x))
+         (offset-x (+ scaled-x *x-offset*)))
+    (floor offset-x)))
 
-;; (redisplay-frame-pane *application-frame*
-;;                         (get-frame-pane *application-frame* 'render-output) :force-p t)
-;; clim:make-rgb-color
+(defun beadie-to-sheet-y (beadie)
+  "Transform the beadie's X Y Z coordinates to the Y coordinates of the sheet to be used by CLIM."
+  (let* ((rotated-y (- (* (y-pos beadie) (cos *ry-theta*))
+                       (* (z-pos beadie) (sin *ry-theta*))))
+         (scaled-y (* *beadie-scale* rotated-y))
+         (offset-y (+ scaled-y *y-offset*)))
+    (floor offset-y)))
 
-(defun draw-circle (knot stream)
-  (let* ((first-beadie (first knot))
-         (x (+ (earth first-beadie) 200))
-         (y (+ (wind first-beadie) 100)))
-    (clim:with-drawing-options (stream :line-thickness 3 :line-cap-shape :round)
-      (clim:draw-text* stream (format nil "test") 20 30)
-      (clim:draw-circle* stream x y 25 :filled nil))))
+(defun draw-beadies (knot stream)
+  "Draw each beadie, and fill it in with color if it's in its solved location."
+  (dolist (beadie (knot-beadies knot))
+    (let ((x (beadie-to-sheet-x beadie))
+          (y (beadie-to-sheet-y beadie))
+          (fillp (untangledp beadie)))
+      (clim:with-drawing-options (stream :line-thickness 2 :line-cap-shape :round)
+        (clim:draw-circle* stream x y *beadie-size* :filled fillp)
+        (when fillp
+          (clim:draw-circle* stream x y (- *beadie-size* 3) :filled t :ink *teal*))))))
 
+(defgeneric draw-threads (knot stream)
+  (:documentation "Draw threads between beadies which are adjacent.")
+  
+  (:method ((knot knot) stream)
+    (dolist (beadie0 (knot-beadies knot))
+      (dolist (beadie1 (knot-beadies knot))
+        (when (threadp beadie0 beadie1)
+          (clim:draw-line* stream 
+                           (beadie-to-sheet-x beadie0)
+                           (beadie-to-sheet-y beadie0)
+                           (beadie-to-sheet-x beadie1)
+                           (beadie-to-sheet-y beadie1)))))))
 
 (defun display-pusheens-home (frame pane)
   "How to display Pusheen's Home."
-  (format pane "~a" (knot frame))
-  (draw-circle (knot frame) pane)
+  (draw-beadies (knot frame) pane)
+  (draw-threads (knot frame) pane)
   (when (untangledp (knot clim:*application-frame*))
     (format pane "Success!")))
 
@@ -282,6 +322,7 @@ The defined function is called ROTATE-AXIS where AXIS is replaced by its value, 
 (defun main (argv)
   "Help untangle Pusheen from her yarn!"
   (declare (ignore argv))
-  (let* ((gameplay-knot (tangle-knot (make-instance 'tiny-knot)))
+  (let* (;(gameplay-knot (tangle-knot (make-instance 'tiny-knot)))
+         (gameplay-knot (make-instance 'tiny-knot))
          (frame (clim:make-application-frame 'pusheens-home :knot gameplay-knot)))
     (clim:run-frame-top-level frame)))
